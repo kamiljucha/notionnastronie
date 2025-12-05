a780a8527f60a43009076b2e0ef2688d211a3941
Kamil <kamiljucha03@gmail.com>
2025-12-05 20:11:47 +0100

    Update [pageId].tsx

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -14,7 +14,8 @@ export const getStaticProps: GetStaticProps<PageProps, Params> = async (
   try {
     const props = await resolveNotionPage(domain, rawPageId)
 
-    return { props, revalidate: 10 }
+    // USUNIĘTO revalidate, bo gryzie się z output: export
+    return { props }
   } catch (err) {
     console.error('page error', domain, rawPageId, err)
 
@@ -40,8 +41,8 @@ export async function getStaticPaths() {
         pageId
       }
     })),
-    // paths: [],
-    fallback: true
+    // ZMIANA NA FALSE - wymagane dla output: export
+    fallback: false
   }
 
   console.log(staticPaths.paths)
