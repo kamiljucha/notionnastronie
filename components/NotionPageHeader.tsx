6b4890825b7c70930e2c305ccb23a456815b4fb7
Kamil <kamiljucha03@gmail.com>
2025-12-05 20:46:54 +0100

    Update NotionPageHeader.tsx

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -3,9 +3,9 @@ import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
 import cs from 'classnames'
 import * as React from 'react'
-import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
+import { Breadcrumbs, Header, useNotionContext } from 'react-notion-x'
 
-import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
+import { navigationLinks, navigationStyle } from '@/lib/config'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import styles from './styles.module.css'
@@ -80,8 +80,6 @@ export function NotionPageHeader({
             .filter(Boolean)}
 
           <ToggleThemeButton />
-
-          {isSearchEnabled && <Search block={block} title={null} />}
         </div>
       </div>
     </header>
