ac6813a9dcabe1521d9dc72364d10ef4788ad27f
Kamil <kamiljucha03@gmail.com>
2025-12-05 20:11:37 +0100

    Remove revalidate from getStaticProps
    Removed revalidate option from getStaticProps to avoid conflict with export output.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,8 @@ export const getStaticProps = async () => {
   try {
     const props = await resolveNotionPage(domain)
 
-    return { props, revalidate: 10 }
+    // USUNIĘTO revalidate, bo gryzie się z output: export
+    return { props }
   } catch (err) {
     console.error('page error', domain, err)
 
