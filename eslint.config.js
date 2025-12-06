ebd878df6ff67ce975e5955a7cbda2c9faa3082b
Kamil <kamiljucha03@gmail.com>
2025-12-06 09:39:16 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,21 @@
-import { config } from '@fisch0920/config/eslint'
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default [
-  ...config,
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
       'react/prop-types': 'off',
-      'unicorn/no-array-reduce': 'off',
-      'unicorn/filename-case': 'off',
-      'unicorn/prefer-global-this': 'off',
       'no-process-env': 'off',
       'array-callback-return': 'off',
       'jsx-a11y/click-events-have-key-events': 'off',
@@ -16,7 +23,15 @@ export default [
       'jsx-a11y/media-has-caption': 'off',
       'jsx-a11y/interactive-supports-focus': 'off',
       'jsx-a11y/anchor-is-valid': 'off',
-      '@typescript-eslint/naming-convention': 'off'
+      'jsx-a11y/alt-text': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn', 
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'react/no-unescaped-entities': 'off', 
+      'react/display-name': 'off',
+      'react-hooks/exhaustive-deps': 'off', 
+      '@next/next/no-img-element': 'off', /
     }
   }
-]
+];
