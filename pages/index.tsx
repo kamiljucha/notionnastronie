592370c0e1d26eb4b0f470d1659a4f1510203198
Kamil <kamiljucha03@gmail.com>
2025-12-06 09:53:28 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,10 @@ export const getStaticProps = async () => {
   try {
     const props = await resolveNotionPage(domain)
 
-    // USUNIĘTO revalidate, bo gryzie się z output: export
     return { props }
   } catch (err) {
     console.error('page error', domain, err)
 
-    // we don't want to publish the error version of this page, so
-    // let next.js know explicitly that incremental SSG failed
     throw err
   }
 }
