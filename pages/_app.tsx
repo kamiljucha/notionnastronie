687088c100d962ecaf25a15dd981891b672c0366
Kamil <kamiljucha03@gmail.com>
2025-12-06 09:54:35 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,8 @@
-// used for rendering equations (optional)
 import 'katex/dist/katex.min.css'
-// used for code syntax highlighting (optional)
 import 'prismjs/themes/prism-coy.css'
-// core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
-// global styles shared across the entire site
 import 'styles/global.css'
-// this might be better for dark mode
-// import 'prismjs/themes/prism-okaidia.css'
-// global style overrides for notion
 import 'styles/notion.css'
-// global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
 
 import type { AppProps } from 'next/app'
